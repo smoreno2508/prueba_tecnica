94f32f83100b5b4e19c393f49e8b082fafd58cf5
smoreno2508 <smoreno2508@gmail.com>
2021-04-29 19:56:24 -0400

    solución problema con estilos

--- www/app/Views/template/header.php
+++ www/app/Views/template/header.php
@@ -8,7 +8,7 @@
     <title>Administrador de usuarios</title>
 
     <!-- Estilos -->
-    <link rel="stylesheet" href="<?= base_url('/assets/css/bootstrap.min.css') ?>" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="<?= base_url('/assets/css/jquery.dataTables.min.css') ?>">
